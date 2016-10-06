0e803479a53707fe532f1c6270ae3a51f61a5335
Techmaster <techmaster@Techmasters-Mac-mini.local>
2016-10-06 18:05:16 +0700

    xong video player

--- src/assets/js/VideoPlayer.js
+++ src/assets/js/VideoPlayer.js
@@ -13,9 +13,12 @@ var VideoPlayer = React.createClass({
     },
     componentDidMount: function () {
         var video = document.getElementById('video');
-        video.addEventListener("pause", this.setPlayerStatus('paused'));
-        video.addEventListener("ended", this.setPlayerStatus('paused'));
-        video.addEventListener("play", this.setPlayerStatus('play'));
+        video.addEventListener("pause", function () {
+            this.setPlayerStatus('paused');
+        }.bind(this));
+        video.addEventListener("play", function () {
+            this.setPlayerStatus('play')
+        }.bind(this));
     },
     setPlayerStatus: function (status) {
         this.setState({
@@ -24,10 +27,11 @@ var VideoPlayer = React.createClass({
     },
     render: function () {
         return (
-            <CardMedia overlay={<CardTitle title={this.props.data.title} subtitle={this.props.data.duration}/>}
-                       overlayContainerStyle={{top: '0', left: '0', right: '0', bottom: 'initial'}}
-                       overlayContentStyle={{height: 'auto', top: '0', left: '0', right: '0', bottom: 'initial'}}
-                       className={"coursePlayer " + this.state.playerStatus}>
+            <CardMedia
+                overlay={<CardTitle className={this.state.playerStatus} title={this.props.data.title} subtitle={this.props.data.duration}/>}
+                overlayContainerStyle={{top: '0', left: '0', right: '0', bottom: 'initial'}}
+                overlayContentStyle={{height: 'auto', top: '0', left: '0', right: '0', bottom: 'initial', padding: '0'}}
+                className="coursePlayer">
                 <video id="video" className="responsive-video" controls autoPlay>
                     <source src={this.props.data.url} type="video/mp4"/>
                 </video>
