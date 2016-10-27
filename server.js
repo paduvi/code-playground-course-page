8e965ac44f7c1f7810c95c599aafd24850fd8482
Phan Đức Việt <viet@techmaster.vn>
2016-10-27 19:11:20 +0700

    sua port

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var compression = require('compression');
 app.use(compression());
 app.use(express.static('static'));
 
-app.listen(2702, "0.0.0.0", function () {
+app.listen(2702, function (err) {
+    if (err)
+        throw err;
     console.log('Example app listening on port 2702!');
 });
